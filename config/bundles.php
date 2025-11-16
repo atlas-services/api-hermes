4a104a558597033fd09b160caced6483dc879724
Tayeb-Chikhi-at-659326164628 <tayeb.chikhi@ceres-advisory.io>
2025-11-16 22:49:26 +0100

    ajout monolog

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
