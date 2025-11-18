12cca38d4e7db35673f3cab952b034bff842b3be
Tayeb-Chikhi-at-659326164628 <tayeb.chikhi@ceres-advisory.io>
2025-11-18 13:33:51 +0100

    test api-x-token

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -16,8 +16,10 @@ class RequestSubscriber implements EventSubscriberInterface
     {
         return [
             KernelEvents::REQUEST => ['onKernelRequest', 10],
+            SecurityEvents::INTERACTIVE_LOGIN => 'onInteractiveLogin',
         ];
 
+        
     }
 
     public function __construct(private EncryptionService $encryptionService, private LoggerInterface $logger)
@@ -28,6 +30,17 @@ class RequestSubscriber implements EventSubscriberInterface
     public function onKernelRequest(RequestEvent $event)
     {
         $request = $event->getRequest();
+        if (strpos($request->getPathInfo(), '/api/templates') === 0) {
+            $this->logger->info('Request Headers api/templates : ', $request->headers->all());
+            $xapikey = $request->headers->get('x-api-key');
+            $this->logger->info('Request Headers x-api-key : ',  [$xapikey]);
+            $authorization = $request->headers->get('authorization');
+            $this->logger->info('Request Headers authorization : ',  [$authorization]);
+            if(is_null($authorization)){
+                $request->headers->set('authorization', $xapikey );
+            }
+            $this->logger->info('Request Headers new header authorization : ',  [$authorization]);
+        }
 
                 // Exclure la route /auth
         if (strpos($request->getPathInfo(), '/auth') === 0) {
@@ -43,6 +56,7 @@ class RequestSubscriber implements EventSubscriberInterface
             }
 
             try {
+
                 $key = base64_decode($data['key']);
 
                 $email = $this->encryptionService->decrypt($data['email'], $data['nonceEmail'], $key);
@@ -62,5 +76,25 @@ class RequestSubscriber implements EventSubscriberInterface
         }
 
     }
+
+
+    public function onInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        $message = 'pas de user';
+        $user = $event->getAuthenticationToken()->getUser();
+        if(!is_null($user)){
+            $message = "email du user : " . $user->getEmail();
+        }
+        $this->logger->log('', " $message ");
+        // Loggez des informations sur l'utilisateur
+        // Utilisez le logger pour voir le nom d'utilisateur ou d'autres détails
+    }
+
+
+
+
+
+
+
 
 }
